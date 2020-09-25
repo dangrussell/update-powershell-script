7aef4734445468f2b518ebced5d3e1cc6f833655
Dan Russell <dan.russell@uconn.edu>
2020-09-25 08:54:05 -0400

    Better spacing in output, removed unvaluable verbosity from choco, moved ncu to last so instructions aren't buried, unaliased start command

--- update.ps1
+++ update.ps1
@@ -3,6 +3,7 @@ $color2 = "DarkRed"
 $color3 = "Red"
 
 Write-Host "[[[UPDATE SCRIPT]]]" -ForegroundColor $color1
+
 Write-Host ""
 Write-Host "..." -ForegroundColor $color3
 Write-Host ""
@@ -53,20 +54,9 @@ Write-Host ""
 
 Write-Host "[Upgrade Chocolatey Packages]" -ForegroundColor $color2
 Write-Host "Upgrading all Chocolatey packages..." -ForegroundColor $color3
-choco upgrade all --yes --verbose
+choco upgrade all --yes
 Write-Host "Done upgrading all Chocolatey packages." -ForegroundColor $color3
-Write-Host ""
-Write-Host "..." -ForegroundColor $color3
-Write-Host ""
 
-###
-
-Write-Host "[npm patch-level updates]" -ForegroundColor $color2
-Write-Host "Checking npm global for patch-level updates..." -ForegroundColor $color3
-Write-Host ""
-npm install -g npm-check-updates
-ncu --global --target patch --loglevel verbose
-Write-Host "Done checking npm global for patch-level updates." -ForegroundColor $color3
 Write-Host ""
 Write-Host "..." -ForegroundColor $color3
 Write-Host ""
@@ -77,6 +67,7 @@ Write-Host ""
 ### Write-Host "Updating PowerShellGet modules (this can be very slow)..." -ForegroundColor $color3
 ### Update-Module -Verbose
 ### Write-Host "Done updating PowerShellGet modules." -ForegroundColor $color3
+
 ### Write-Host ""
 ### Write-Host "..." -ForegroundColor $color3
 ### Write-Host ""
@@ -91,13 +82,15 @@ $className = "MDM_EnterpriseModernAppManagement_AppManagement01"
 $methodName = "UpdateScanMethod"
 Get-CimInstance -Namespace $namespaceName -ClassName $className -Verbose | Invoke-CimMethod -MethodName $methodName -Verbose
 
+Write-Host "Opening Downloads and Updates in Microsoft Store..." -ForegroundColor $color3
+# shell:appsFolder\Microsoft.WindowsStore_8wekyb3d8bbwe!App
+Start-Process ms-windows-store://downloadsandupdates
+
 Write-Host "Done updating all Microsoft Store apps." -ForegroundColor $color3
+
 Write-Host ""
 Write-Host "..." -ForegroundColor $color3
 Write-Host ""
-Write-Host "Opening Downloads and Updates in Microsoft Store..." -ForegroundColor $color3
-# shell:appsFolder\Microsoft.WindowsStore_8wekyb3d8bbwe!App
-start ms-windows-store://downloadsandupdates 
 
 ###
 
@@ -112,12 +105,26 @@ Write-Host "Running Windows Update and Microsoft Update..." -ForegroundColor $co
 #Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d
 Write-Host ""
 Get-WindowsUpdate -MicrosoftUpdate -Install -AcceptAll -Verbose
+
+Write-Host "Opening Windows Update in Settings..." -ForegroundColor $color3
+Start-Process ms-settings:windowsupdate-action
+
 Write-Host "Done running Windows Update and Microsoft Update." -ForegroundColor $color3
+
 Write-Host ""
 Write-Host "..." -ForegroundColor $color3
 Write-Host ""
-Write-Host "Opening Windows Update in Settings..." -ForegroundColor $color3
-start ms-settings:windowsupdate-action
+
+###
+
+Write-Host "[npm patch-level updates]" -ForegroundColor $color2
+Write-Host "Checking npm global for patch-level updates..." -ForegroundColor $color3
+Write-Host ""
+npm install -g npm-check-updates
+ncu --global --target patch --loglevel verbose
+Write-Host "Done checking npm global for patch-level updates." -ForegroundColor $color3
+
+Write-Host ""
 Write-Host "..." -ForegroundColor $color3
 Write-Host ""
 
