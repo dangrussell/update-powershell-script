c80ea912caf286cf01e8ceea1bcafca58515d379
Dan Russell <4387475+dangrussell@users.noreply.github.com>
2021-04-14 09:14:35 -0400

    WSL Ubuntu commented out, add error helper text, refreshenv uncommented

--- update.ps1
+++ update.ps1
@@ -19,6 +19,7 @@ Write-Host ""
 # TODO: Check for wsl installation before using wsl
 # TODO: Add list of default/recommend apt packages to install on first run
 #>
+<#
 $wslName = "WSL Ubuntu"
 $wslUser = wsl whoami
 Write-Host "[Update, upgrade, and autoremove in $wslName]" -ForegroundColor $color2
@@ -57,6 +58,7 @@ Write-Host ""
 Write-Host "..." -ForegroundColor $color3
 Write-Host ""
 #endregion
+#>
 
 #region Chocolatey packages
 <#
@@ -73,6 +75,7 @@ choco upgrade all --yes
 Write-Host ""
 
 Write-Host "Done upgrading all Chocolatey packages." -ForegroundColor $color3
+Write-Host "If you encountered any 'already referencing a newer version' errors, try running this: choco upgrade all --yes --ignore-dependencies" -ForegroundColor $color3
 Write-Host ""
 
 Write-Host "..." -ForegroundColor $color3
@@ -187,17 +190,15 @@ Write-Host ""
 #endregion
 
 #region Finish & Clean-Up
-<#
 Write-Host "[Finish & Clean-Up]" -ForegroundColor $color2
 Write-Host ""
 
-Write-Host "Refreshing environment variables..." -ForegroundColor $color3
+# Write-Host "Refreshing environment variables..." -ForegroundColor $color3
 refreshenv
 Write-Host ""
 
 Write-Host "..." -ForegroundColor $color3
 Write-Host ""
-#>
 #endregion
 
 #region Done
