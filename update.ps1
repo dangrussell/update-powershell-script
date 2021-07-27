b80b3307f70f7a7596904fabd3da2ef4ea41918d
Dan Russell <4387475+dangrussell@users.noreply.github.com>
2021-07-27 11:00:49 -0400

    Highlight suggested command with new color

--- update.ps1
+++ update.ps1
@@ -4,6 +4,7 @@ Set-Location ~
 $color1 = "DarkMagenta"
 $color2 = "DarkRed"
 $color3 = "Red"
+$color4 = "Cyan"
 #endregion
 
 #region Opening
@@ -75,7 +76,7 @@ Write-Host ""
 Write-Host "Done upgrading all Chocolatey packages." -ForegroundColor $color3
 Write-Host ""
 Write-Host "If you encountered any 'already referencing a newer version' errors, try running this:"
-Write-Host "choco upgrade all --yes --ignore-dependencies"
+Write-Host "choco upgrade all --yes --ignore-dependencies" -ForegroundColor $color4
 Write-Host ""
 
 Write-Host "..." -ForegroundColor $color3
